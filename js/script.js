57c3f087d36320c7bda70d2e95242e2fc26ec306
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-10 14:51:27 +0200

    inserito nome/cognome/colore preferito su js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
-// Commento singola riga
-
 /*
 
-    Commento multipla riga
+    Step da seguire:
+    
+    1. Chiedo Nome/Cognome/Colore preferito all'utente e mi salvo il valore
+    2. Comunichiamo il risultato
 
 */
+
+// NOME
+
+// 1. Chiedo Nome/Cognome/Colore preferito all'utente e mi salvo il valore
+const name = prompt('Qual è il tuo nome?')
+console.log('Nome:', name)
+
+// 2. Comunichiamo il risultato
+document.getElementById('name-text').innerHTML = 'Il tuo Nome è ' + name;
+
+// COGNOME
+
+// 1. Chiedo Nome/Cognome/Colore preferito all'utente e mi salvo il valore
+const surname = prompt('Qual è il tuo cognome?')
+console.log('Cognome:', surname)
+
+// 2. Comunichiamo il risultato
+document.getElementById('surname-text').innerHTML = 'Il tuo Cognome è ' + surname;
+
+// COLORE PREFERITO
+
+// 1. Chiedo Nome/Cognome/Colore preferito all'utente e mi salvo il valore
+const color = prompt('Qual è il tuo colore preferito?')
+console.log('Colore:', color)
+
+// 2. Comunichiamo il risultato
+document.getElementById('color-text').innerHTML = 'Il tuo Colore preferito è ' + color;
